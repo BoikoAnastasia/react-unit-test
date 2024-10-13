473d0d96f8e3a68e069193e0cf3a978271a247b2
Анастасия Бойко <anastasia151000@rambler.ru>
2024-10-13 21:20:02 +0300

    basic tests components

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState("");
+    useEffect(() => {
+        setTimeout(() => {
+            setData({});
+        }, 100);
+    }, []);
+
+    const onClick = () => setToggle(toggle => !toggle)
+
+    return (
+        <div>
+            <h1 data-testId="input-elem">{value}</h1>
+            {toggle && <div data-testid="toggle-elem">toggle</div>}
+            {data && <div> data</div>}
+            <h1>Hello</h1>
+            <button data-testid="toggle-btn" onClick={onClick}>Click me</button>
+            <input onChange={e=>setValue(e.target.value)} type="text" placeholder="input value" />
+        </div>
+    );
+};
+
+export default App;
